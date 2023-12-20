b83210bbf631d6fe1d37378699272936ab5eee81
“yangguanghe” <young742674@gmail.com>
2023-12-21 00:43:17 +0800

    update

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h2>{{$t("home.header")}}</h2>
+    <p>{{$t("home.content",{company:"DuYi-Edu"})}}</p>
+    <p>{{$t("home.num_visits", 20)}}</p>
+    <p>{{ $d(new Date(), "longFormat") }}</p>
+  </main>
+</template>
+<script setup lang="ts">
+// 下面的引入已被全局变量替换
+// import { useI18n } from 'vue-i18n';
+// const { t } = useI18n();
+</script>
